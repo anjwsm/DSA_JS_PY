c71135890b66fdac0833e55e4d32304b46edd9a6
anjwsm <anjalikushwahawsm@gmail.com>
2023-08-11 23:56:07 +0530

    for loop practice

---
+++ HelloWorld.js
@@ -0,0 +1,59 @@
+// for loop
+// for(var i =1;i<=10;i++){
+//     console.log(i);
+// }
+
+// if else
+
+// var age = 14;// assignment operator
+
+// if(age <18){
+//     console.log('Your age is not fit for this app');
+                                          
+    // console.log('Please come after '+  (18-age)  +  ' year'); // string concatanation(addition)
+// }
+// else if(age ==18){ // equate operator
+//     console.log('Your age is just perfect ');
+//     console.log('Welcome!');
+// }
+// else{
+//    console.log('Welcome!'); 
+// }
+
+// array DS 
+
+// var arr=[4, 56,10, 40900,99,505,67,10001,293,383493];?\
+// index -> 0  , 1 ,  2, 3
+// accessing with index
+// console.log(arr[2])
+
+// var sum =0;
+// for(var i=0;i<arr.length;i=i+1){
+//     sum =sum +arr[i];
+
+// }
+// console.log(sum);
+
+
+// switch statement
+
+  var x=10;
+
+// discrete (not continuous)
+  switch(x){
+    case 10 : {
+        console.log('You are too small kiddy');
+        break;       
+    }
+    case 16: {
+        console.log('Grow up dude');
+        break;
+    }
+    case 18:{
+        console.log('You age is just perfect, Welcome');
+        break;
+    }
+    default :{
+        console.log('Restricted');
+    }
+  }
