08fb2ac92305297242cf64ae7f9f5c24c05d3bfc
anjwsm <anjalikushwahawsm@gmail.com>
2023-08-12 22:12:21 +0530

    Max functions

--- DSA/array.js
+++ DSA/array.js
@@ -1,1 +1,33 @@
 // array questions
+function getTheMaxOfThreeNumber(a,b,c){
+    if (a>=b && a>=c) { 
+        return a;
+    }
+    if (b>=a && b>=c){
+        return b;
+    }
+    if (c>=a && c>=b){
+        return c;
+    }
+    if (a=b=c){
+        return a,b,c;
+    }
+
+}
+// console.log(getTheMaxOfThreeNumbers(2,1,2))
+
+function getMax(a,b){
+   return a>b?a:b;
+}
+// console.log(getTheMaxOfTwoNumbers(5,5))
+
+function getTheMaxOfThreeNumbers(a,b,c){
+    
+
+
+    return getMax(a, getMax(b,c));
+    
+}
+console.log(getTheMaxOfThreeNumbers(829,9,0))
+
+
