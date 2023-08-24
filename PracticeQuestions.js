635c8981ef2f83f02a3981b19b32d2248ac9aafa
anjwsm <anjalikushwahawsm@gmail.com>
2023-08-24 20:00:15 +0530

    Array functions

--- PracticeQuestions.js
+++ PracticeQuestions.js
@@ -213,12 +213,12 @@ for (let i =1; i<= n; i++){
 
 
 // function printPyramids(n){
- for (i=1 ; i <= n ; i++){
-    for(let j = 1 ; j <= 2 *i-1 ; j ++){
-        process.stdout.write('*');
-    }
-    process.stdout.write('\n');
- }
+ // for (i=1 ; i <= n ; i++){
+ //   for(let j = 1 ; j <= 2 *i-1 ; j ++){
+ //       process.stdout.write('*');
+ //   }
+ //   process.stdout.write('\n');
+ // }
 // }
 // printPyramids(5);
 
@@ -240,14 +240,16 @@ for (let i =1; i<= n; i++){
   //  process.stdout.write('\n');
 // }
 
-// let n = 5 ;
-// for ( i = 1 ; i <= n ; i++){
-   // let aCode = A.charCodeA+(0);
-   // for(j=1 ; j <= i ; j++){
-    //    process.stdout.write(String.fromCharCode(aCode + i - 1));
-   // }
-   //  process.stdout.write ('\n');
+ // function letterPyramid(n){
+//  for ( i = 1 ; i <= n ; i++){
+  //   let aCode = 'A'.charCodeA+(0);
+  //  for(j=1 ; j <= i ; j++){
+ //  process.stdout.write(String.fromCharCode(aCode + i - 1));
+  //  }
+  //    process.stdout.write ('\n');
+//  }
 // }
+//  console.log(letterPyramid(5));
 
 
 
--- String.js
+++ String.js
@@ -22,9 +22,40 @@
 // 'b' in object1
 // '20' in object1
 
-const Employee = {
-    firstName : 'John',
-    lastName :'Doe',
-} ;
-delete Employee.firstName;
-console.log(Employee.firstName);
+// const Employee = {
+  //  firstName : 'John',
+   // lastName :'Doe',
+}
+// } ;
+//delete Employee.firstName;
+// console.log(Employee.firstName);
+
+
+
+// const animals = ['dog','horse' , 'lion','cat'];
+// animals.push('cow', 'goat');
+// console.log(animals);
+// console.log(animals.join());
+
+
+// const array1 = [5,12,8.,976,55];
+// const found = array1.find((element) => element > 10);
+// console.log(found);
+
+
+// const array1 = [1,3,5,7];
+// const map1 = array1.map((x) => x*2);
+// console.log(map1);
+
+
+
+
+
+// const str = 'Apple is red';
+// const words = str.split(' ');
+// console.log(words[2]);
+// const chars= str.split('');
+// console.log(chars[7]);
+
+
+
